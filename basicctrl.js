76baf379eb6a5827f498ac992c6eb376a15f7fea
i5ting <i5ting@126.com>
2016-12-10 22:53:06 +0800

    lazycommmit, please ignore

--- basicctrl.js
+++ basicctrl.js
@@ -8,7 +8,7 @@ module.exports = class MyBasicController extends BasicController {
   }
   
   get() { 
-    var a = this.query.a
+    let a = this.query.a
     // this.renderType='view'
     return {
       a: 'this is a',
